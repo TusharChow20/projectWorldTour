ae75727e3143c89b2fd591a1b88a278930cb4644
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-09-20 13:08:47 +0600

    first react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import Countries from './components/Countries'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React world</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
